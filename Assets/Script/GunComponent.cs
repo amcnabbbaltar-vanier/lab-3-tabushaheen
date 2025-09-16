981bbe9d5274b68da97c45d76f8fddedb89209f4
tabushaheen <tarek.abushaheen@gmail.com>
2025-09-15 21:43:53 -0400

    Final Submission
    Full implementation
    - Bullet component created with proper tags
    - Created BulletComponent script
    - Implemented GunComponent script
    - Created TargetComponent script
    - Implemented GameManager script to increment score

--- Assets/Script/GunComponent.cs
+++ Assets/Script/GunComponent.cs
@@ -7,7 +7,34 @@ public class GunComponent : MonoBehaviour
     public float bulletMaxImpulse = 10.0f;
     public float maxChargeTime = 3.0f;
     private float chargeTime = 0.0f;
-    private bool isCharging = false;
 
-   
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            chargeTime = 0f;
+        }
+
+        if (Input.GetButton("Fire1"))
+        {
+            chargeTime += Time.deltaTime;
+            chargeTime = Mathf.Clamp(chargeTime, 0f, maxChargeTime);
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            ShootBullet();
+        }
+    }
+
+
+    void ShootBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+
+        float bulletImpulse = (chargeTime / maxChargeTime) * bulletMaxImpulse; 
+
+        rb.AddForce(bulletSpawnPoint.forward * bulletImpulse, ForceMode.Impulse);
+    }
 }
